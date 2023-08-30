3e66bd1ddef4c9c157ab0b1f61fc190a8df6c56d
ArifKhan <tech.aarifkhan@gmail.com>
2023-08-30 10:34:07 +0530

    navbar fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-signal-example';
+  showNavBar = signal(false);
+
+  toggleNavbar() {
+    this.showNavBar.update(val => !val);
+  }
 }
